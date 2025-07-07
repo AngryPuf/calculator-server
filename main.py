0692c80589464cc7ca6749cf61ac2c8ad9f32f25
AngryPuf <josefalt2020@gmail.com>
2025-07-07 01:28:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
+from flask_cors import CORS
 import math
 
 app = Flask(__name__)
+CORS(app)  # <-- This enables CORS for all routes
 
 @app.route("/")
 def home():
@@ -25,3 +27,4 @@ def calc():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=81)
+
